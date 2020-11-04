8005a9dbafad4f7a5ae685fc645c3435459cb5e9
paul <paul.th.cartier@gmail.com>
2020-11-04 10:51:11 +0100

    try catch schema

--- src/controllers/wilders.js
+++ src/controllers/wilders.js
@@ -1,59 +1,51 @@
 const WilderModel = require("../models/Wilder");
 
 module.exports = {
-  create: (req, res) => {
-    WilderModel.init().then(() => {
+  create: async (req, res) => {
+    try {
+      await WilderModel.init();
       const newWilder = new WilderModel(req.body);
-      newWilder
-        .save()
-        .then((result) => {
-          res.status(201).json({ result });
-        })
-        .catch((err) => {
-          console.log(err);
-          res.status(500).json({ err });
-        });
-    });
+      const wilderSaved = await newWilder.save();
+      res.status(201).json({ wilderSaved });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ err });
+    }
   },
 
-  getAll: (_, res) => {
-    WilderModel.find()
-      .then((wilders) => {
-        res.status(200).json({ wilders });
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json({ err });
-      });
+  getAll: async (_, res) => {
+    try {
+      const wilders = await WilderModel.find();
+      res.status(200).json({ wilders });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ err });
+    }
   },
 
-  update: (req, res) => {
-    WilderModel.findByIdAndUpdate(req.params.id, req.body, { new: true })
-      .then((wilderUpdated) => {
-        if (wilderUpdated) {
-          res.status(200).json({ wilderUpdated });
-        } else {
-          res.status(400).send("Unknow id");
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json({ err });
-      });
+  update: async (req, res) => {
+    try {
+      const wilderUpdated = await WilderModel.findByIdAndUpdate(req.params.id, req.body, { new: true });
+      if (!wilderUpdated) {
+        return res.status(400).send("Unknow id");
+      }
+      res.status(200).json({ wilderUpdated });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ err });
+    }
   },
 
-  delete: (req, res) => {
-    WilderModel.findByIdAndDelete(req.params.id)
-      .then((wilderDeleted) => {
-        if (wilderDeleted) {
-          res.status(200).send(`${wilderDeleted.name} has been deleted`);
-        } else {
-          res.status(400).send("Unknow id");
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json({ err });
-      });
+  delete: async (req, res) => {
+    try {
+      const wilderDeleted = WilderModel.findByIdAndDelete(req.params.id);
+      if (!wilderDeleted) {
+        return res.status(400).send("Unknow id");
+      }
+      res.status(200).send(`${wilderDeleted.name} has been deleted`);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ err });
+    }
   },
 };
